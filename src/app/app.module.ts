11d9d53128de47adeeec4730576af3573f9832a8
Fabian Stroh <fabian.stroh@andrena.de>
2023-03-05 17:40:10 +0100

    made page accessible on github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StatlineComponent } from './statline/statline.component';
+import { AttributeComponent } from './attribute/attribute.component';
+import {TooltipModule} from "ng2-tooltip-directive";
+import { HerodetailsComponent } from './herodetails/herodetails.component';
+import { TypuspickerComponent } from './typuspicker/typuspicker.component';
+import { RuestungpickerComponent } from './ruestungpicker/ruestungpicker.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { WaffenpickerComponent } from './waffenpicker/waffenpicker.component';
+import { WaffenschadenlineComponent } from './waffenschadenline/waffenschadenline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatlineComponent,
+    AttributeComponent,
+    HerodetailsComponent,
+    TypuspickerComponent,
+    RuestungpickerComponent,
+    WaffenpickerComponent,
+    WaffenschadenlineComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TooltipModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ruestungpicker/ruestungen.ts
@@ -0,0 +1,39 @@
+import {Ruestung} from "./ruestung";
+
+export const ruestungen: Ruestung[] = [
+  {
+    name: "Lendenschurz",
+    RS: 0,
+    minusGE: 0,
+  },
+  {
+    name: "Normale Kleidung",
+    RS: 1,
+    minusGE: 0,
+  },
+  {
+    name: "Wattierter Waffenrock",
+    RS: 2,
+    minusGE: 0,
+  },
+  {
+    name: "Lederrüstung",
+    RS: 3,
+    minusGE: 0,
+  },
+  {
+    name: "Kettenhemd",
+    RS: 4,
+    minusGE: -1,
+  },
+  {
+    name: "Schuppenpanzer",
+    RS: 5,
+    minusGE: -1,
+  },
+  {
+    name: "Ritterrüstung",
+    RS: 6,
+    minusGE: -2,
+  },
+]
